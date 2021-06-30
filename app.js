c6af044e168a4b8ab84bdf41bbcdcc7391b055d0
pree208 <preethiashok208@gmail.com>
2021-06-29 22:47:19 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Init Github
+const github = new Github;
+
+//init ui
+
+const ui = new UI;
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if (userText !== '') {
+    // Make http call
+    github.getUser(userText)
+      .then(data => {
+
+        if (data.profile.message === 'Not Found') {
+          // Show alert
+          ui.showAlert('Profile not found', 'alert alert-danger');
+
+        } else {
+          // Show profile
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
+        }
+      })
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+}); 
